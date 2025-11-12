3877d197c6647685681f517ef31207e6cd03190f
kt-git-1 <taniguchidev33@gmail.com>
2025-11-12 21:37:09 +0900

    update UserAlreadyExistsException

--- backend/src/main/java/com/example/backend/error/users/UserAlreadyExistsException.java
+++ backend/src/main/java/com/example/backend/error/users/UserAlreadyExistsException.java
@@ -1,11 +1,10 @@
 package com.example.backend.error.users;
 
-
 import com.example.backend.error.DomainError;
 import com.example.backend.error.DomainException;
 
 public class UserAlreadyExistsException extends DomainException {
-    public UserAlreadyExistsException(String email) {
-        super(DomainError.USER_ALREADY_EXISTS, "user.already_exists", email);
+    public UserAlreadyExistsException(String username) {
+        super(DomainError.USER_ALREADY_EXISTS, "user.already_exists", username);
     }
 }
